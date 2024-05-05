6d46cef2343ee7b5431e85ce3d397f4893357852
MohakKhatri <mohakk.mk@gmail.com>
2024-05-05 17:05:24 +0530

    check_manager updated

--- Redesigning Poor Code/check_manager.py
+++ Redesigning Poor Code/check_manager.py
@@ -8,52 +8,47 @@ from menus import user_id_error_menu
 
 class Check_manager:
     def __init__(self, check_db_path="C:/Users/MOHAK/Desktop/TASK/Redesigning Poor Code/json/check_db.json"):
-        self.user_store = Check_store(check_db_path)
+        self.check_store = Check_store(check_db_path)
         self.log = Log()
         self.input_validator = Input_validator()
 
     def check_in(self):
-        name, user_id = self.input_validator.empty_string_validator("Enter user name: "), self.input_validator.empty_string_validator("Enter user ID: ")
-        new_user = Check(name, user_id)
+        isbn, user_id = self.input_validator.empty_string_validator("Enter user name: "), self.input_validator.empty_string_validator("Enter user ID: ")
+        new_check = Check(isbn, user_id)
         try:
-            self.user_store.store_add_user(new_user)
-            self.log.add_log(f"User_manager: success, add_user(ID:{new_user.get_id()})")
-            return f"User_manager: success, add_user(ID:{new_user.get_id()})"
+            self.check_store.store_checkin(new_check)
+            self.log.add_log(f"Check_manager: success, checkin(ID:{new_check.get_user_id()}, ISBN:{new_check.get_book_isbn()})")
+            return f"Check_manager: success, checkin(ID:{new_check.get_user_id()}, ISBN:{new_check.get_book_isbn()})"
         except ValueError as e:
             print(f"\n\nERROR: {e}", "Failed to add user.", sep="\n")
-            return f"User_manager: failed: add_user(ID:{new_user.get_id()})"
+            return f"Check_manager: failed: checkin(ID:{new_check.get_user_id()}, ISBN:{new_check.get_book_isbn()})"
 
-    def get_check_from_user_id(self):
-        user_id = self.input_validator.empty_string_validator("Enter user ID to search: ")
-        self.log.add_log(f"User_manager: success, get_user_from_id({user_id})")
-        response=self.user_store.store_get_user_id(user_id)
-        if response=="User ID not found.":
+    def get_check_from_user_id(self, user_id):
+        self.log.add_log(f"Check_manager: success, get_check_from_user_id({user_id})")
+        response=self.check_store.store_get_check_user_id(user_id)
+        if response=="This user has no checkins":
             return response
-        return response.JSONize()
+        return [i.JSONize() for i in response]
 
-    def get_check_from_book_isbn(self):
-        name = self.input_validator.empty_string_validator("Enter user name to search: ")
-        response = self.user_store.store_get_user_name(name)
-        if response != "User name not found.":
-            self.log.add_log(f"User_manager: success, get_user_from_name({name})")
-            return f"The users with names that matched with the query are: {response}"
-        self.log.add_log(f"User_manager: failure, get_user_from_name({name})")
-        return response
+    def get_check_from_book_isbn(self, isbn):
+        self.log.add_log(f"Check_manager: success, get_user_from_name({isbn})")
+        response = self.check_store.store_get_check_book_isbn(isbn)
+        if response=="This book hasn't been checked in.":
+            return response
+        return [i.JSONize() for i in response]
 
     def get_all_checks(self):
-        response = self.user_store.store_get_all()
-        self.log.add_log("User_manager: success, get_all_users()")
+        response = self.check_store.store_get_all()
+        self.log.add_log("Check_manager: success, get_all_checks()")
         if response == []:
-            return "No users to display."
-        return f"List of all users:{response}"
+            return "No chccks to display."
+        return f"List of all checks:{response}"
 
-    def check_out(self):
-        user_id = self.input_validator.empty_string_validator("Enter user ID: ")
+    def check_out(self, user_id, book_isbn):
         try:
-            self.user_store.store_delete_user(user_id)
-            self.log.add_log(f"User_manager: success, delete_user(ID:{user_id})")
-            return f"User_manager: success, delete_user(ID:{user_id})"
-        except ValueError as e:
-            print(f"\n\nERROR: {e}", "Failed to delete user.", sep="\n")
-            self.log.add_log(f"User_manager: failure, delete_user(ID:{user_id})")
-            return f"User_manager: failure, delete_user(ID:{user_id})"
+            check=self.check_store.store_get_check(user_id, book_isbn)
+            self.check_store.store_check_out(check)
+            self.log.add_log(f"Check_manager: success, check_out({check.get_user_id, check.get_book_isbn})")
+            return f"Check_manager: success, check_out({check.get_user_id, check.get_book_isbn})"
+        except:
+            return f"Check_manager: success, check_out({check.get_user_id, check.get_book_isbn})"
